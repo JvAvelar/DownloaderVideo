5555b197041ca09fb165c885ddf0d7247cbd1d38
JvAvelar <jvitoravelar1200@gmail.com>
2023-02-24 15:12:33 -0300

    update main.py
    tirei algumas linhas

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import pytube
 
 link = input('Digite a URL do YouTube: ')
-
 yt = pytube.YouTube(link)
-
 yt.streams.first().download('D:\Downloads\Videos')
-
 print(f'Download do vídeo: {yt.title} , realizado com sucesso.')
